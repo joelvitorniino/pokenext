7cf61428a137d39e7b80eda44adf054b7d50c162
joelvitorniino <joelprofessor13@gmail.com>
2022-12-09 21:39:16 -0300

    call poke api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,43 @@
+import { InferGetStaticPropsType } from 'next';
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+interface PokeAPI {
+  results: [{
+    name: string,
+    id: number
+  }]
+}
+
+export async function getStaticProps() {
+  const maxPokemons = 251;
+  const api = 'https://pokeapi.co/api/v2/pokemon';
+
+  const res = await fetch(`${api}?limit=${maxPokemons}`);
+  const data: PokeAPI = await res.json();
+
+  // add pokemon index
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
+}
+
+export default function Home({ pokemons }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <h1>PokeNext</h1>
-  )
+    <div>
+      <h1>PokeNext</h1>
+      <ul>
+        {pokemons.map((pokemon) => {
+          return (
+            <li key={pokemon.id}>{pokemon.name}</li>
+          )
+        })}
+      </ul>
+    </div>
+  );  
 }
