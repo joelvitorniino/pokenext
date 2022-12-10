3da4107af5866a179a5b15cd9f9a2a678a8e9822
joelvitorniino <joelprofessor13@gmail.com>
2022-12-09 21:30:09 -0300

    page of system

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import styles from '../styles/About.module.css'
+
+export default function About() {
+    return (
+        <div className={styles.about}>
+            <h1>Sobre o projeto</h1>
+            <p>Lorem, ipsum dolor</p>
+            <Image src="/images/charizard.png" width="300" height="300" alt="Charizard" />
+        </div>
+    );
+}
